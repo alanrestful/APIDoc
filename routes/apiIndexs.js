846232d7f6ee2994082bf7cc7b022d01afe1c586
MichaelZhao <MichaelZhaoZero@gmail.com>
2016-09-08 14:30:08 +0800

    add show json view

--- routes/apiIndexs.js
+++ routes/apiIndexs.js
@@ -1,10 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var rf = require("fs");
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  console.log("message");
   res.render('index_layout', { title: 'Express' });
+});
+
+router.get('/api_doc', function(req, res) {
+  var data = rf.readFileSync(__dirname + "/swagger.json","utf-8");
+
+  res.send(data);
 });
 
 module.exports = router;
