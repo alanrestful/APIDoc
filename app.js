1acea7c7698c909f35ccac6002e9a714eaca4f3e
栾博文 <lbw@terminus.io>
2016-09-12 10:34:24 +0800

    app.js add login filter

--- app.js
+++ app.js
@@ -7,11 +7,15 @@ var bodyParser = require('body-parser');
 // hbs渲染
 var exphbs  = require('express-handlebars');
 
+var session = require('express-session');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var projects = require('./routes/projects');
 var apiIndexs = require('./routes/apiIndexs');
 var apiManager = require('./routes/apiManager');
+var connect = require('connect');
+var MongoStore = require('connect-mongo')(session);
 
 var app = express();
 
@@ -30,8 +34,32 @@ app.use(favicon());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
-app.use(cookieParser());
+app.use(cookieParser('123'));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'anywhere',
+    resave: true,
+    saveUninitialized: true,
+    store: new MongoStore({
+        url : 'mongodb://localhost/apidoc',
+        ttl : 60 * 60,
+    })
+}));
+
+/// session
+app.use(function(req,res,next){
+    // console.log(JSON.stringify(req.session));
+    var url = req.originalUrl;
+    if (req.session.user){
+        next();
+    } else {
+        if (url !== '/'){
+            res.send();
+            res.redirect('/');
+        }
+    }
+    next();
+});
 
 app.use('/', routes);
 app.use('/users', users);
@@ -45,6 +73,7 @@ app.use(function(req, res, next) {
     err.status = 404;
     next(err);
 });
+
 
 /// error handlers
 
@@ -71,6 +100,8 @@ app.use(function(err, req, res, next) {
 });
 
 
+
+
 module.exports = app;
 
 var server = app.listen(9010, function () {
@@ -79,4 +110,4 @@ var server = app.listen(9010, function () {
   var port = server.address().port;
 
   console.log("应用实例，访问地址为 http://%s:%s", host, port)
-})
+});
