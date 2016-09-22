94ed214d5d656cf86ac6a4cd6978795bb5d3a529
hehaiyangwork <hehaiyangwork@qq.com>
2016-09-22 09:20:42 +0800

    Fixed: project manager styles

--- models/Project.js
+++ models/Project.js
@@ -3,17 +3,17 @@ var Schema = mongoose.Schema;
 
 //Project schema
 var _Project = new Schema({
+    _id : {type : String},
     name : {type : String, default : ''},
     type : {type : Number, default : 1},
-    tag : {type : String, default : ''},
-    creator : {type : String, default : ''},
+    owner : {type : String, default : ''},
     created_at : {type : Date, default: Date.now},
     updated_at : {type : Date, default: Date.now}
 });
 
-_Project.methods.findByName = function(name, callback) {
+_Project.method('findByName', function(name ,callback){
     return this.model('api_projects').find({name: name}, callback);
-}
+});
 
 // export Project
 exports.Project = mongoose.model('api_projects', _Project);
