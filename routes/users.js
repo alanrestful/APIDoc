0a0a2a6155f22b65e8995539e6257b81d1b4b3e2
hehaiyangwork <hehaiyangwork@qq.com>
2016-09-22 15:37:25 +0800

    Added: user manager

--- routes/users.js
+++ routes/users.js
@@ -10,36 +10,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function(req, res) {
 
-  // var swaggerWithCallback = new SwaggerClient({
-  //     url: "http://petstore.swagger.io/v2/swagger.json",
-  //     success: function () {
-  //       res.send(swaggerWithCallback);
-  //         // console.log('callback ready', swaggerWithCallback);
-  //     }
-  // });
-
-  // the post options
-    // var optionspost = {
-    //     host : 'jidd.com',
-    //     port : '80',
-    //     path : '/v2/api-docs?group=api',
-    //     method : 'GET'
-    // };
-    //
-    // // do the POST call
-    // // 服务器端发送REST请求
-    // var reqPost = http.request(optionspost, function(resPost) {
-    //     resPost.on('data', function(d) {
-    //         res.send(d);
-    //     });
-    // });
-    // reqPost.end();
-    // reqPost.on('error', function(e) {
-    //     console.error(e);
-    // });
-
-
-    res.send('respond with a resource');
+    res.render('user/user_manager', {title: '用户管理'});
 });
 
 router.post('/login', function (req, res) {
