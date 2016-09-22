742557d1b3c614ae71471f8e7a5ff0616199c491
hehaiyangwork <hehaiyangwork@qq.com>
2016-09-22 15:30:48 +0800

    Added: project manager

--- routes/projects.js
+++ routes/projects.js
@@ -7,33 +7,59 @@ var application = require('../models/Application').Application;
 /* 项目列表 */
 router.get('/', function(req, res) {
   project.find(function(err, projects) {
-    if(!req.query.id){
-      res.render('projects/project_manager', {projects: projects});
-    }else{
-      var pid = req.query.id;
-      application.find({"projectId": pid}, function (err, applications){
-        res.render('projects/project_manager', {projects: projects, applications: applications, pid: pid });
-      });
-    }
+    res.render('projects/project_manager', {projects: projects});
   });
+});
+
+/* 获取项目应用 */
+router.get('/apps', function(req, res) {
+  if(!req.body.id && !req.body.env){
+    console.log(req.body);
+    application.find({projectId: req.body.id, env: req.body.env}, function (err, applications){
+      res.json(applications);
+    });
+  }else{
+    res.json({});
+  }
 });
 
 /* 创建项目 */
 router.post('/', function(req, res) {
   var params = {
     name: req.body.name,
-    tag: req.body.tag,
-    creator: req.session.user
+    owner: req.session.user,
+    env_json: req.body.env_json
   };
-
-  project.create(params, function(error) {
-    if(error) {
-      console.log('create project error:%s', error);
+  var result = {};
+  project.create(params, function(err) {
+    if(err) {
+      result.status = false;
+      result.messages = error;
+      console.log('create project error:%s', err);
     } else {
+      result.status = true;
+      result.messages = '';
       console.log('create project success!');
     }
+    res.json(result);
   });
-  res.redirect('/projects');
+});
+
+/* 删除项目 */
+router.delete('/', function(req, res) {
+  var result = {};
+  project.remove({_id: req.body.id}, function(err) {
+    if(err) {
+      result.status = false;
+      result.messages = err;
+      console.log('delete project error:%s', err);
+    } else {
+      result.status = true;
+      result.messages = '';
+      console.log('delete project success!');
+    }
+    res.json(result);
+  });
 });
 
 module.exports = router;
