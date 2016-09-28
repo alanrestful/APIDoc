aac71ae34539df1a6686efbbaf26476279cb866e
栾博文 <lbw@terminus.io>
2016-09-28 15:13:37 +0800

    rm require module

--- models/UpdateLogs.js
+++ models/UpdateLogs.js
@@ -3,8 +3,6 @@
  */
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var Paging = require('../helpers/paging').Paging;
-
 var _UpdateLog = new Schema({
     applicationId: {type: String},
     author: {type: String},
