ebdca982b39d18a73a0bdecd80d6ac977399c433
hehaiyangwork <hehaiyangwork@qq.com>
2016-09-28 12:00:45 +0800

    Fixed: case

--- routes/cases.js
+++ routes/cases.js
@@ -8,28 +8,62 @@ var ConanCaseData = require('../models/ConanCaseData').ConanCaseData;
 var router = express.Router();
 
 /* 创建用例 */
-router.post('/', function(req, res) {
-  var data = req.body;
-  var conanGroup = new ConanGroup({
-    pid: data.pid,
-    name: data.name,
-    flag: data.flag
-  });
-  conanGroup.save();
+router.post('/group', function(req, res) {
 
-  var conanCaseModel = new ConanCaseModel({
+  // 1.创建模版组 2.模版 3.用例组 4.用例
+  // 5.用例 6.数据
+  /*
+  pid: 项目id
+  tempGroup: 模版组
+  tempName: 模版名称
+  caseGroup: 用例组 ~
+  caseName: 用例名称 ~
+  fragment:
+    [{rela_path:xxxxx, elements:[[{hash:xxxx, tagName: tagName,
+    type:type, id: id, className: className, name: name,
+    value:value, placeholder:placeholder, baseURI:baseURI,
+    innerText:innerText, href:href, xPath:”xxxx”, isFormEl: true},{一个页面的第二个element},{}]]}, {第二个页面}]
+  data: [[{m_hash:xxx, value:sherry, expect:xx},{一个页面的第二个数据}],[第二个页面里的数据]] ～
+  */
+
+  var data = req.body;
+  var tempGroup = new ConanGroup({
     pid: data.pid,
-    name: data.name,
-    fragment: data.flag
+    name: data.tempGroup,
+    isCase: false
   });
-  conanCaseModel.save();
+  tempGroup.save();
+
+  var tempModel = new ConanCaseModel({
+    pid: data.gid,
+    name: data.tempName,
+    fragment: data.fragment
+  });
+  tempModel.save();
+
+  if(data.caseGroup && data.caseName){
+    var caseGroup = new ConanGroup({
+      pid: data.pid,
+      name: data.caseGroup,
+      isCase: true
+    });
+    caseGroup.save();
+
+    var caseModel = new ConanCaseModel({
+      pid: data.gid,
+      name: data.caseName,
+      fragment: data.fragment
+    });
+    caseModel.save();
+  }
 
   var conanCaseData = new ConanCaseData({
-    mid: data.pid,
+    mid: data.mid,
     name: data.name,
     data: data.data
   });
   conanCaseData.save();
+
   res.json({status: true, messages: null,result: null});
 });
 
