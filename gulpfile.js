9b221062435496670e0010ce5824bc659e0eebb6
hehaiyangwork <hehaiyangwork@qq.com>
2016-09-29 14:42:18 +0800

    Fixed: case manager

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp'),
     sass = require('gulp-sass'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
-    gulpSequence = require('gulp-sequence');
+    gulpSequence = require('gulp-sequence'),
+    exec = require('child_process').exec;
 
 //创建test任务
 gulp.task('test', function(){
@@ -21,6 +22,15 @@ gulp.task('watchSass', function(){
   gulp.watch('public/stylesheets/app.scss', ['sass']);
 });
 
-gulp.task('w', function(callback){
-  gulpSequence('sass', 'watchSass', callback);
+
+gulp.task('run', function (cb) {
+  exec('supervisor app.js', function (err, stdout, stderr) {
+    console.log(stdout);
+    console.log(stderr);
+    cb(err);
+  });
+})
+
+gulp.task('serve', function(callback){
+  gulpSequence('run', 'sass', 'watchSass', callback);
 })
--- public/javascripts/case.js
+++ public/javascripts/case.js
@@ -1,14 +1,29 @@
 "use strict";
 
 $(function(){
+  load();
   $(document).on('click', '.edit-setting', editSettingEvent);
-  $(document).on('submit', '.edit-setting-form', saveSettingEvent);
+  $(document).on('submit', '.edit-env-form', saveSettingEvent);
   $(document).on('click', '.edit-group', editGroupEvent);
   $(document).on('click', '.del-group', delGroupEvent);
   $(document).on('click', '.edit-detail', editDetailEvent);
   $(document).on('click', '.del-detail', delDetailEvent);
-  $(document).on('change', '#setting-name', settingNameOnchangeEvent);
+  $(document).on('change', '#setting-name', settingChangeEvent);
 });
+
+var load = function(event){
+  var id = localStorage.getItem('case-pid');
+  var name = localStorage.getItem('case-name');
+  var env = localStorage.getItem('case-env');
+  var domain = localStorage.getItem('case-domain');
+  if(id && name && env && domain){
+    $('.setting-wait').addClass('hide');
+    $('.setting-name').removeClass('hide');
+    $('.setting-env').removeClass('hide');
+    $('.setting-name').html(name);
+    $('.setting-env').html(env);
+  }
+}
 
 /* 编辑设置 */
 var editSettingEvent = function(event){
@@ -19,9 +34,10 @@ var editSettingEvent = function(event){
     success: function(data){
       if(data.status){
         $("#setting-name").html('');
+        $("#setting-env").html('<option>请选择</option>');
         var html = '<option>请选择</option>';
         for(var i in data.result){
-          html = html + '<option value="'+ data.result[i]._id +'" data-env="'+ JSON.stringify(data.result[i].env_json) +'">'+ data.result[i].name +'</option>'
+          html = html + '<option value="'+ data.result[i]._id + "," + data.result[i].name +'" data-env=\''+ JSON.stringify(data.result[i].env_json) +'\'>'+ data.result[i].name +'</option>'
         }
         $("#setting-name").append(html);
       }
@@ -30,14 +46,44 @@ var editSettingEvent = function(event){
   $('#editSettingModal').modal('show');
 };
 
-var settingNameOnchangeEvent = function(event){
+var settingChangeEvent = function(event){
   event && event.preventDefault();
-  alert(JSON.stringify($(event.currentTarget)));
+  var env = $("#setting-name").find("option:selected").data('env');
+  var html = '';
+  for(var i in env){
+    var name;
+    switch(env[i].name){
+      case 'dev':
+        name = '开发环境';
+        break;
+      case 'test':
+        name = '测试环境';
+        break;
+      case 'pre':
+        name = '预发环境';
+        break;
+      case 'prod':
+        name = '生产环境';
+        break;
+      default:
+        break;
+    }
+    html = html + '<option value="'+ name + "," + env[i].domain +'">'+ name +'</option>'
+  }
+  $("#setting-env").html(html);
 };
 
 var saveSettingEvent = function(event){
   event && event.preventDefault();
-  $('#editSettingModal').modal('show');
+  var data = $(event.currentTarget).serializeJSON();
+  var arr1 = data.id.split(",");
+  var arr2 = data.env.split(",");
+  localStorage.setItem("case-pid", arr1[0]);
+  localStorage.setItem("case-name", arr1[1]);
+  localStorage.setItem("case-env", arr2[0]);
+  localStorage.setItem("case-domain", arr2[1]);
+  $('#editSettingModal').modal('hide');
+  window.location.reload();
 };
 
 /* 编辑分组 */
