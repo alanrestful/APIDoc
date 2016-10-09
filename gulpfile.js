be3a9391b405f04601a6726b9e58fa748b3aeb93
hehaiyangwork <hehaiyangwork@qq.com>
2016-10-09 12:02:46 +0800

    fixed: somethings

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,5 @@
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    gulpSequence = require('gulp-sequence'),
-    exec = require('child_process').exec;
-
-//创建test任务
-gulp.task('test', function(){
-  console.log("this is a test task");
-});
+    sass = require('gulp-sass');
 
 // 编译sass
 gulp.task('sass', function(){
@@ -18,20 +9,6 @@ gulp.task('sass', function(){
 });
 
 // sass监听
-gulp.task('watchSass', function(){
+gulp.task('w', function(){
   gulp.watch('public/stylesheets/app.scss', ['sass']);
 });
-
-// 启动express
-gulp.task('run', function (cb) {
-  exec('supervisor app.js', function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
-})
-
-// 启动服务
-gulp.task('serve', function(callback){
-  gulpSequence('run', 'sass', 'watchSass', callback);
-})
